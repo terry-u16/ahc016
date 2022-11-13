a5f1b2c7b0edc646351e7cb7b0e2dc0085ad606b
terry-u16 <terry-u16@users.noreply.github.com>
2022-11-13 13:48:33 +0900

    出力高速化

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use encoders::{clique::CliqueEncoder, Encoder};
 #[allow(unused_imports)]
 use proconio::*;
-#[allow(unused_imports)]
-use rand::prelude::*;
-
+use std::io::{self, BufWriter, Write as _};
 mod encoders;
 mod graph;
 mod utils;
@@ -11,13 +9,18 @@ mod utils;
 #[derive(Debug, Clone)]
 struct Input {}
 
+#[proconio::fastout]
 fn main() {
     let encoder = CliqueEncoder::new(100);
+    let stdout = io::stdout();
+    let stdout = &mut BufWriter::new(stdout.lock());
+
+    writeln!(stdout, "{}", encoder.graph_size()).unwrap();
 
     for i in 0..100 {
         let graph = encoder.encode(i);
-        eprintln!("{}", &graph);
+        writeln!(stdout, "{}", &graph).unwrap();
     }
 
-    eprintln!("{}", encoder.graph_size());
+    stdout.flush().unwrap();
 }
